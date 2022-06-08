8ee7864d4267b308a0f3a754e35117e12fb0614b
Mike Perrenoud <MPerrenoud@kloveair1.com>
2022-06-08 06:51:22 -0600

    Initial commit.

---
+++ AmericanBeefMarket.Web/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AmericanBeefMarket.Web.Data
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+		public string Name { get; set; }
+
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+		public string Address { get; set; }
+	}
+}
